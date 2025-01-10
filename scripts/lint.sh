8445c2cbe8c88273da4e4b859647d3b67e49fa9e
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2025-01-10 03:19:26 +0100

    Add chart-testing as pre-commit hook

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,9 +7,9 @@ REPO_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 echo -e "\n-- Linting all Helm Charts --\n"
 docker run \
     --rm \
-    --workdir=/data \
-    --volume $REPO_ROOT:/data \
-    --entrypoint /bin/sh \
+    --workdir="//data" \
+    --volume "/${REPO_ROOT}:/data" \
+    --entrypoint //bin/sh \
     quay.io/helmpack/chart-testing:v3.11.0 \
     -c "
         git config --global --add safe.directory /data && \
